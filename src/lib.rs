a77ea08f78f7b2e131e5f3c4c290539eeb37f8cc
amadeusdotpng <amadeus8182@gmail.com>
2024-12-28 17:01:15 -0600

    The Surface

---
+++ src/lib.rs
@@ -0,0 +1,225 @@
+use winit::{
+    event::*,
+    event_loop::EventLoop,
+    keyboard::{KeyCode, PhysicalKey},
+    window::{WindowBuilder, Window},
+};
+
+struct State<'a> {
+    surface: wgpu::Surface<'a>,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    size: winit::dpi::PhysicalSize<u32>,
+
+    // must be declared after surface for some weird reason
+    // https://sotrh.github.io/learn-wgpu/beginner/tutorial2-surface/
+    window: &'a Window,
+
+    clear_color: [f64; 3],
+}
+
+impl<'a> State<'a> {
+    async fn new(window: &'a Window) -> State<'a> {
+        let size = window.inner_size();
+
+        // instance is a handle to GPU
+        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::PRIMARY,
+            ..Default::default()
+        });
+
+        let surface = instance.create_surface(window).unwrap();
+
+        let adapter = instance.request_adapter(
+            &wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(),
+                compatible_surface: Some(&surface),
+                force_fallback_adapter: false,
+            }
+        ).await.unwrap();
+
+
+        let (device, queue) = adapter.request_device(
+            &wgpu::DeviceDescriptor {
+                required_features: wgpu::Features::empty(),
+                required_limits: wgpu::Limits::default(),
+                label: None,
+                memory_hints: Default::default(),
+            },
+            None
+        ).await.unwrap();
+
+        let surface_caps = surface.get_capabilities(&adapter);
+
+        // shader code assumes sRGB surface texture
+        let surface_format = surface_caps.formats.iter()
+            .find(|f| f.is_srgb())
+            .copied()
+            .unwrap_or(surface_caps.formats[0]);
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: size.width,
+            height: size.height,
+            present_mode: surface_caps.present_modes[0],
+            alpha_mode: surface_caps.alpha_modes[0],
+            view_formats: vec![],
+            desired_maximum_frame_latency: 2,
+        };
+
+        Self {
+            window,
+            surface,
+            device,
+            queue,
+            config,
+            size,
+            clear_color: [0.009021491898012131, 0.009021491898012131, 0.023103556157921437]
+
+        }
+    }
+
+    pub fn window(&self) -> &Window {
+        &self.window
+    }
+
+    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        if new_size.width > 0 && new_size.height > 0 {
+            self.size = new_size;
+            self.config.width = new_size.width;
+            self.config.height = new_size.height;
+            self.surface.configure(&self.device, &self.config);
+        }
+    }
+
+    fn input(&mut self, event: &WindowEvent) -> bool {
+        match event {
+            WindowEvent::CursorMoved { position, .. } => {
+                self.clear_color = [
+                    (position.x / (self.size.width as f64)).powf(2.2),
+                    (position.y / (self.size.width as f64)).powf(2.2),
+                    0.023103556157921437
+                ];
+                true
+            }
+            _ => false
+        }
+    }
+
+    fn update(&mut self) {
+    }
+
+    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        let output = self.surface.get_current_texture()?;
+
+        let view = output.texture.create_view(&wgpu::TextureViewDescriptor::default());
+
+        let mut encoder = self.device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+            label: Some("Render Encoder")
+        });
+
+        let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            label: Some("Render Pass"),
+            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                view: &view,
+                resolve_target: None,
+                ops: wgpu::Operations {
+                    load: wgpu::LoadOp::Clear(wgpu::Color {
+                        r: self.clear_color[0],
+                        g: self.clear_color[1],
+                        b: self.clear_color[2],
+                        a: 1.0,
+                    }),
+                    store: wgpu::StoreOp::Store,
+                },
+            })],
+            depth_stencil_attachment: None,
+            occlusion_query_set: None,
+            timestamp_writes: None,
+        });
+
+        drop(_render_pass);
+
+        self.queue.submit(std::iter::once(encoder.finish()));
+        output.present();
+
+        Ok(())
+    }
+}
+
+pub fn from_linear_rgb(c: [f32; 3]) -> u32 {
+    let f = |x: f32| -> u32 {
+        let y = if x > 0.0031308 {
+            let a = 0.055;
+            (1.0 + a) * x.powf(-2.4) - a
+        } else {
+            12.92 * x
+        };
+        (y * 255.0).round() as u32
+    };
+    f(c[0]) << 16 | f(c[1]) << 8 | f(c[2])
+}
+
+pub async fn run() {
+    env_logger::init();
+    let event_loop = EventLoop::new().unwrap();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    let mut state = State::new(&window).await;
+    let mut surface_configured = false;
+
+
+    let _ = event_loop.run(move |event, control_flow| {
+        match event {
+            Event::WindowEvent {
+                ref event,
+                window_id
+            } if window_id == state.window.id() => if !state.input(event) {
+                match event {
+                    WindowEvent::CloseRequested |
+                    WindowEvent::KeyboardInput {
+                        event: KeyEvent {
+                            state: ElementState::Pressed,
+                            physical_key: PhysicalKey::Code(KeyCode::Escape),
+                            ..
+                        },
+                        ..
+                    } => control_flow.exit(),
+                    WindowEvent::Resized(physical_size) => {
+                        log::info!("physical_size: {physical_size:?}");
+                        surface_configured = true;
+                        state.resize(*physical_size);
+                    }
+                    WindowEvent::RedrawRequested => {
+                        state.window().request_redraw();
+
+                        if !surface_configured { return; }
+
+                        state.update();
+                        match state.render() {
+                            Ok(_) => {}
+                            // reconfigure the surface if it's lost or outdated
+                            Err(
+                                wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated,
+                            ) => state.resize(state.size),
+
+                            // system is out of memory so quit
+                            Err(wgpu::SurfaceError::OutOfMemory) => {
+                                log::error!("OutOfMemory");
+                                control_flow.exit();
+                            }
+
+                            // happens when frame takes too long to present
+                            Err(wgpu::SurfaceError::Timeout) => {
+                                log::warn!("Surface Timeout");
+                            }
+                        }
+                    }
+                    _ => {}
+                }
+            },
+            _ => {}
+        }
+    });
+}
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+use wgpu_triangle::run;
+
+fn main() {
+    pollster::block_on(run());
+}
